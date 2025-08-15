6ebdbfd88f3fda2056946120d89dd941a6ad4ed7
dayattt111 <tumbaldaya@gmail.com>
2025-08-15 18:22:26 +0800

    add: installation docker arch

---
+++ install.sh
@@ -0,0 +1,19 @@
+# update repo and package arch
+sudo pacman -Syu
+
+# instal docker package manager
+sudo pacman -S docker
+
+echo "okai docker well done installations"
+
+# start docker 
+sudo systemctl start docker.service
+
+# enable to automations docker
+sudo systemctl enable docker.service
+
+# add group users
+# sudo usermod -aG docker $USER
+
+# test users menu
+# newgrp docker
